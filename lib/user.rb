8c4dc5e9730675def3cfa87a3c2784537a01a6d9
Swishyfishie <pclandreiuk@gmail.com>
2020-01-14 05:09:13 +0000

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,14 @@
+class User
+    
+    attr_accessor :first_name, :last_name 
+
+    # def initialize(first_name, last_name)
+    #     @first_name = first_name
+    #     @last_name = last_name 
+    #     @knowledge = []
+    # end
+
+    # def learn(knowledge) 
+    #     @knowledge << knowledge
+    # end 
+end
